97591523811f17b7fb5e2b226ab97f95ac0e9fec
gonzaloiv <gonzalo.iglesias@teimas.com>
2017-01-10 10:45:25 +0100

    utilizar el piece pooler para la generación controlada de piezas

--- Assets/Scripts/Entities/PiecePooler.cs
+++ Assets/Scripts/Entities/PiecePooler.cs
@@ -5,27 +5,38 @@ using System.Collections.Generic;
 public class PiecePooler : MonoBehaviour {
   
   // VARIABLES
-  private List<GameObject> pooledPieces = new List<GameObject> ();
+  private List<GameObject> pooledPieces = new List<GameObject>();
+  private List<int> pooledPiecesIndexes = new List<int>();
   private PieceFactory pieceFactory;
   private GameObject piece;
 
   // MONO BEHAVIOUR
-  public void Awake() {
+  void Awake() {
     pieceFactory = GetComponent<PieceFactory>();
-    for (int i = 0; i < GlobalConstants.PieceTypeAmount; i++) {
-      // TODO: Que no se creen de manera aleatoria sino mediante algún algoritmo que haga unas piezas más regulares que otras
-      piece = pieceFactory.CreatePiece(i) as GameObject;
-      piece.SetActive(false);
-      pooledPieces.Add(piece);
+    for (int i = 0; i < GlobalConstants.InitialPooledPiecesAmount; i++) {
+      for (int j = 0; j < GlobalConstants.PieceTypeAmount; j++) {
+        // TODO: Que no se creen de manera aleatoria sino mediante algún algoritmo que haga unas piezas más regulares que otras
+        piece = pieceFactory.CreatePiece(j) as GameObject;
+        piece.SetActive(false);
+        pooledPieces.Add(piece);
+      }
     }
   }
 
   // PUBLIC BEHAVIOUR
-  public GameObject GetPiece() {
-    for (int i = 0; i < pooledPieces.Count; i++) {
+  public GameObject GetRandomPiece() {
+    int randomIndex;
+    while (pooledPiecesIndexes.Count < pooledPieces.Count) {
+      do { randomIndex = Random.Range(0, GlobalConstants.PieceTypeAmount); } 
+      while (!pooledPiecesIndexes.Contains(randomIndex));
+
       if (!pooledPieces[i].activeInHierarchy) 
+        pooledPiecesIndexes.Clear();
         return pooledPieces[i];
+
+      pooledPiecesIndexes.Add(randomIndex); 
     }  
+
     piece = pieceFactory.CreatePiece() as GameObject;
     piece.SetActive(false);
     pooledPieces.Add(piece);  
--- Assets/Scripts/Managers/BoardManager.cs
+++ Assets/Scripts/Managers/BoardManager.cs
@@ -7,34 +7,28 @@ public class BoardManager : MonoBehaviour {
   public GameObject boardPrefab;
 
   // VARIABLES
-  private PieceFactory pieceFactory;
+  private PiecePooler piecePooler;
   private GameObject activePiece;
   private GameObject board;
 
   // MONO BEHAVIOUR
   void Awake() {
-    pieceFactory = GetComponent<PieceFactory>();
+    piecePooler = GetComponent<PiecePooler>();
     board = Instantiate(boardPrefab) as GameObject;
 
     activePiece = new GameObject("ActivePiece");
     activePiece.transform.parent = gameObject.transform;
 
     // test de creación de piezas
-    // TODO: dependerá de las collisions con el grupo de piezas en el tablero
     StartCoroutine(NextPiece());
   }
-
-  // TODO: rutina de la partida
-  void OnEnable() {}
-
-  void OnDisable() {}
-
+ 
   // PRIVATE BEHAVIOUR
   private IEnumerator NextPiece() {
     for (int i = 0; i < GlobalConstants.PieceTypeAmount; i++) {
       Destroy(activePiece.GetComponent<PieceController>());
-      activePiece = pieceFactory.CreatePiece(i) as GameObject;
-      activePiece.transform.position = GlobalConstants.BoardCenter;
+      activePiece = piecePooler.GetRandomPiece() as GameObject;
+      activePiece.SetActive(true);
 
       yield return new WaitForSeconds(2);
     }
